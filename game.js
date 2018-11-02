3a9d503f383020547978cb984b8b48259dcd268e
alanbutcher <alan.butcher81@gmail.com>
2018-11-02 15:02:07 -0600

    Part 1 complete

--- game.js
+++ game.js
@@ -1,22 +1,29 @@
 let health = 100
+let targetName = 'Thanos'
+let hits = 0
 
 
 function slap() {
   health--;
+  hits++;
   //alert(health);
   update()
 }
 function punch() {
-  health--;
+  health -= 5;
+  hits++;
   update()
 }
 function kick() {
-  health--;
+  health -= 10;
+  hits++;
   update()
 }
 
 
 function update() {
   document.getElementById('health').innerText = health.toString()
+  document.getElementById('Thanos').innerText = targetName
+  document.getElementById('hits').innerText = hits.toString()
 }
 update()
