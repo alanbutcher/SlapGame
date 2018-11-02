eebac893a48a18956bd6e77c3bd668725227b7b1
alanbutcher <alan.butcher81@gmail.com>
2018-11-02 16:40:14 -0600

    modiier function added

--- game.js
+++ game.js
@@ -1,29 +1,52 @@
 let target = {
   targetName: 'Thanos',
   health: 100,
-  hits: 0
+  hits: 0,
+  items: [],
+  totalMods: 0
+}
+
+let items = {
+  powerStone: { name: 'Power Stone', modifier: 2, description: "Don't let it touch the ground!" },
+  realityStone: { name: 'Reality stone', modifier: 3, description: 'Wait, say what?' },
+  timeStone: { name: 'Time stone', modifier: 4, description: 'Come back with me, Marty!'}
+}
+
+function giveStone() {
+  items.push(target.items);
+}
+
+function addMods() {
+  let out = 0
+  for (let index = 0; index < target.items.length; index++) {
+    const item = target.items[index];
+    out += item.modifier
+  }
+  target.totalMods = out
 }
 
 
-
-
 function slap() {
-  target.health--;
+  addMods()
+  target.health -= 1 + target.totalMods;
   target.hits++;
   //alert(health);
   update()
 }
+
 function punch() {
-  target.health -= 5;
-  target.hits++;
-  update()
-}
-function kick() {
-  target.health -= 10;
+  addMods()
+  target.health -= 5 + target.totalMods;
   target.hits++;
   update()
 }
 
+function kick() {
+  addMods()
+  target.health -= 10 + target.totalMods;
+  target.hits++;
+  update()
+}
 
 function update() {
   document.getElementById('health').innerText = target.health.toString()
